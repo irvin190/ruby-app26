5983ae4b0e62d75138d2dbca56b39628ecadfc85
Irvin <irvinrodr24@gmail.com>
2016-03-11 20:03:21 -0500

    Heroku fix

---
+++ db/migrate/20160311012939_fixing_heroku.rb
@@ -0,0 +1,4 @@
+class FixingHeroku < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160309231513) do
+ActiveRecord::Schema.define(version: 20160311012939) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
